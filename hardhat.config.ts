a1dc6476344b0bb2cc122c9a5e8636ea0387f564
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-07-08 21:39:18 +0800

    feat: add abi exporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import './toolbox'
+import 'hardhat-abi-exporter'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
+  abiExporter: {
+    path: './data/abi',
+    clear: true,
+    runOnCompile: true
+  },
 }
 
 export default config
