424cfafe34f7b76d20fc64699fd326976a0644d4
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-07-15 23:44:43 +0800

    feat: basic lottery

---
+++ contracts/Lottery/LotteryMachine.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import "../interfaces/IRandomNumberGenerator.sol";
+
+contract LotteryMachine {
+    enum Status {
+        Pending,
+        Open,
+        Close,
+        Claimable
+    }
+
+    mapping(address => uint32) private _userWithTicketNumber;
+    uint32 public finalNumber;
+    Status public status;
+    IRandomNumberGenerator public rng;
+
+    constructor(address rngAddress) {
+        rng = IRandomNumberGenerator(rngAddress);
+        status = Status.Pending;
+    }
+
+    function startLottery() external {
+        status = Status.Open;
+    }
+
+    function buyTicket(uint32 ticketNumber) external {
+        _userWithTicketNumber[msg.sender] = ticketNumber;
+    }
+
+    function closeLottery() external {
+        status = Status.Close;
+        rng.getRandomNumber();
+    }
+
+    function drawTheFinalNumber() external {
+        uint256 randomResult = rng.viewRandomResult();
+        uint32 finalNumber_ = uint32(1000000 + (randomResult % 1000000));
+        finalNumber = finalNumber_;
+    }
+
+    function claimTicket() external {
+        uint32 ticketNumber = _userWithTicketNumber[msg.sender];
+        uint256 rewards = _calculateRewards(ticketNumber);
+        // todo: transfer the rewards to the user
+    }
+
+    function _calculateRewards(uint32 ticketNumber) internal view returns (uint256) {
+        // todo: calculate rewards for some ticket number
+        return 0;
+    }
+}
