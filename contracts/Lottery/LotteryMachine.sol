801c24a5bdbf9d292671dd331fa519ac2d7cff7a
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-07-28 15:56:55 +0800

    feat: add basic validation

--- contracts/Lottery/LotteryMachine.sol
+++ contracts/Lottery/LotteryMachine.sol
@@ -7,6 +7,18 @@ import "../libraries/Ownable.sol";
 // import "hardhat/console.sol";
 
 contract LotteryMachine is Ownable {
+    error InvalidRngAddress();
+    error InvalidStartLotteryStatus(Status status);
+    error InvalidInjectFundsStatus(Status status);
+    error InvalidBuyTicketStatus(Status status);
+    error InvalidCloseLotteryStatus(Status status);
+    error InvalidDrawNumberStatus(Status status);
+    error InvalidClaimStatus(Status status);
+    error FinalNumberHasBeenDrawn();
+    error InvalidRewardBreakdown(uint256[6] rewardsBreakdown);
+    error InvalidPriceInToken(uint256 price);
+    error InvalidInjectRngStatus(Status status);
+
     enum Status {
         Pending,
         Open,
@@ -20,7 +32,7 @@ contract LotteryMachine is Ownable {
     uint256[6] private _rewardsBreakdown;
     uint256 private _amountCollected;
     mapping(uint32 => uint32) private _bracketCalculator;
-    uint256 private _priceTicketInCake;
+    uint256 private _priceTicketInToken;
     uint32 public finalNumber;
     Status public status;
     IRandomNumberGenerator public rng;
@@ -38,6 +50,12 @@ contract LotteryMachine is Ownable {
     }
 
     function injectRng(address rngAddress) external onlyOwner {
+        if (status != Status.Pending && status != Status.Open) {
+            revert InvalidInjectRngStatus(status);
+        }
+        if (rngAddress == address(0)) {
+            revert InvalidRngAddress();
+        }
         rng = IRandomNumberGenerator(rngAddress);
         rng.acceptOwnership();
     }
@@ -49,25 +67,46 @@ contract LotteryMachine is Ownable {
 
     function startLottery(
         uint256[6] calldata rewardsBreakdown,
-        uint256 priceTicketInCake
+        uint256 priceTicketInToken
     ) external onlyOwner {
+        if (status != Status.Pending) {
+            revert InvalidStartLotteryStatus(status);
+        }
+        if (false) {
+            revert InvalidRewardBreakdown(rewardsBreakdown);
+        }
         status = Status.Open;
         _rewardsBreakdown = rewardsBreakdown;
-        _priceTicketInCake = priceTicketInCake;
+        _priceTicketInToken = priceTicketInToken;
     }
 
     function buyTicket(uint32 ticketNumber) external {
+        if (status != Status.Open) {
+            revert InvalidBuyTicketStatus(status);
+        }
         _userWithTicketNumber[msg.sender] = ticketNumber;
-        _amountCollected += _priceTicketInCake;
-        slt.transferFrom(msg.sender, address(this), _priceTicketInCake);
+        _amountCollected += _priceTicketInToken;
+        slt.transferFrom(msg.sender, address(this), _priceTicketInToken);
     }
 
     function closeLottery() external onlyOwner {
+        if (status != Status.Open) {
+            revert InvalidCloseLotteryStatus(status);
+        }
         status = Status.Close;
         rng.requestRandomNumber();
     }
 
     function drawFinalNumberAndMakeLotteryClaimable() external onlyOwner {
+        if (status != Status.Close) {
+            revert InvalidDrawNumberStatus(status);
+        }
+        if (address(rng) == address(0)) {
+            revert InvalidRngAddress();
+        }
+        if (finalNumber != 0) {
+            revert FinalNumberHasBeenDrawn();
+        }
         uint256 randomResult = rng.viewResult();
         uint32 finalNumber_ = uint32(1000000 + (randomResult % 1000000));
         finalNumber = finalNumber_;
@@ -75,6 +114,9 @@ contract LotteryMachine is Ownable {
     }
 
     function claimTicket(uint32 bracket) external {
+        if (status != Status.Claimable) {
+            revert InvalidClaimStatus(status);
+        }
         uint32 ticketNumber = _userWithTicketNumber[msg.sender];
         uint256 rewards = _calculateRewards(ticketNumber, bracket);
         if (rewards > 0 && rewards <= _amountCollected) {
